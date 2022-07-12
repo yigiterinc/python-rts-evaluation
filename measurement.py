7e75cf59731a9bd26686a83d29d09604f06c1574
Yigit Kemal Erinc <erincyigit@gmail.com>
2022-07-12 15:14:13 +0200

    Implement measurements

--- measurement.py
+++ measurement.py
@@ -4,14 +4,16 @@ from git import Repo
 
 from ttictoc import tic, toc
 
-target_folders = ["aws-dynamodb-encryption-python"]
+target_folders = ["boto3"]
 
-CSV_HEADERS = ["Iteration", "Time taken without RTS", "Time taken with RTS", "Saved Time", "Ratio (with/without RTS)"]
+CSV_HEADERS = ["Iteration", "Time taken without RTS", "Number of Changed Files", "Percentage of Selected Tests",
+               "Time taken with RTS", "Saved Time", "Ratio (with/without RTS)"]
+
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-NUMBER_OF_COMMITS_TO_CHECK = 19
+NUMBER_OF_COMMITS_TO_CHECK = 20
 
 
 def init():
@@ -28,85 +30,98 @@ def measure_testing_times_in_commit_history(folder):
     __cleanup()
 
     # write headers to csv file if it doesn't exist
-    if not os.path.exists("../output.csv"):
-        write_csv_headers()
+    if os.path.exists(f"../{folder}-output.csv"):
+        # delete file
+        os.remove(f"../{folder}-output.csv")
+
+    write_csv_headers(folder)
 
     global NUMBER_OF_COMMITS_TO_CHECK
 
-    # Find commit id of 19 commits back
     repo = Repo(os.getcwd())
 
-    try:
-        while NUMBER_OF_COMMITS_TO_CHECK > 0:
-            commits = repo.iter_commits('master', max_count=NUMBER_OF_COMMITS_TO_CHECK)
-            commit_id = next(commits).hexsha
-            # print commit id
-            logging.info(f"Iter: {20 - NUMBER_OF_COMMITS_TO_CHECK}, Commit id: {commit_id}")
+    commit_ids = []
+    # iterate over commits
+    for commit in repo.iter_commits('master', max_count=NUMBER_OF_COMMITS_TO_CHECK, reverse=True, no_merges=True):
+        commit_ids.append(commit.hexsha)
 
-            # Checkout to commit id
-            repo.git.checkout(commit_id)
+    # checkout to commit_ids[0]
+    repo.git.checkout(commit_ids[0])
+    run_test_suite_build_mapping_database()
+    counter = 1
+    while counter < len(commit_ids):
+        commit_id = commit_ids[counter]
+        logging.info(f"Iter: {counter}, Commit id: {commit_id}")
 
-            commit_id_of_next_commit = next(commits).hexsha
+        os.system(f"git cherry-pick -n {commit_id}")
+        logging.info(f"Applied commit {commit_id}")
 
-            # Apply the next commit after commit_id with --continue using os
-            os.system(f"git cherry-pick {commit_id_of_next_commit}")
+        change_list_names = get_changelist_of_commit(repo)
+        number_of_changed_files = len(change_list_names)
+        # log changed file names
+        logging.info("Changed files: " + str(change_list_names))
 
-            logging.info(f"Applied commit {commit_id_of_next_commit}")
-            logging.info("Printing git staged file names")
-            os.system("git diff --cached --name-only")
-            diff_list = repo.head.commit.diff()
-            # log diff list
-            for diff in diff_list:
-                logging.info(f"{diff.a_path}")
+        # run git diff --cached
+        logging.info("Git status")
+        os.system("git status")
 
-            run_test_suite_build_mapping_database()
+        # Run tests and note how much time it took
+        tic()
+        os.system("pytest --junitxml=retest-all.xml -q")
 
-            # Run tests and note how much time it took
-            tic()
-            os.system("pytest -q")
+        # Calculate the time it took to run the tests
+        time_taken = toc()
+        log.info("Time taken to run tests without RTS: " + str(time_taken))
 
-            # Calculate the time it took to run the tests
-            time_taken = toc()
-            log.info("Time taken to run tests without RTS: " + str(time_taken))
+        # Run tests with RTS and note how much time it took
+        tic()
+        # --co
+        os.system(f"pytest --rts --rts-coverage-db=pytest-rts-coverage -q --junitxml=rts.xml")
+        time_taken_with_rts = toc()
+        # Calculate the time it took to run the tests
+        log.info("Time taken to run tests with RTS: " + str(time_taken_with_rts))
+        number_of_selected_tests = 0
 
-            # Run tests with RTS and note how much time it took
-            tic()
-            os.system("pytest -q --rts --rts-coverage-db=.coverage")
-            time_taken_with_rts = toc()
-            # Calculate the time it took to run the tests
-            log.info("Time taken to run tests with RTS: " + str(time_taken_with_rts))
+        saved_time = time_taken - time_taken_with_rts
+        log.info("Saved time: " + str(saved_time))
 
-            # Gain of time
-            saved_time = time_taken - time_taken_with_rts
-            log.info("Gain of time: " + str(saved_time))
+        if time_taken == 0:
+            log.info("Time taken is 0, skipping...")
+            return
 
+        # Ratio
+        ratio = time_taken_with_rts / time_taken
+        log.info("Ratio: " + str(ratio))
 
-            if time_taken == 0:
-                log.info("Time taken is 0, skipping...")
-                return
+        # Write to file
+        write_to_csv_file(folder, f"{counter},{time_taken},"
+                                  f"{number_of_changed_files}, {number_of_selected_tests},"
+                                  f"{time_taken_with_rts},{saved_time},{ratio}")
 
-            # Ratio
-            ratio = time_taken_with_rts / time_taken
-            log.info("Ratio: " + str(ratio))
-
-            # Write to file
-            write_to_csv_file(f"{20 - NUMBER_OF_COMMITS_TO_CHECK},{time_taken},{time_taken_with_rts},{saved_time},{ratio}")
-
-            NUMBER_OF_COMMITS_TO_CHECK -= 1
-    except StopIteration:
-        log.info("No more commits to check")
+        # Clean up
+        # git reset hard
+        os.system("git reset --hard")
+        os.system("git restore --staged .")
+        # get current position of head
+        repo.git.checkout(commit_id, force=True)
+        counter += 1
 
     calculate_average_statistics(folder)
 
 
-def write_csv_headers():
-    with open("../output.csv", "a") as f:
+def get_changelist_of_commit(repo):
+    changed_file_names = [item.a_path for item in repo.index.diff('Head')]
+    return changed_file_names
+
+
+def write_csv_headers(folder):
+    with open(f"../{folder}-output.csv", "a") as f:
         f.write(",".join(CSV_HEADERS))
         f.write("\n")
 
 
-def write_to_csv_file(what_to_write):
-    with open("../output.csv", "a") as f:
+def write_to_csv_file(folder, what_to_write):
+    with open(f"../{folder}-output.csv", "a") as f:
         f.write(what_to_write)
         f.write("\n")
 
@@ -117,33 +132,46 @@ def calculate_average_statistics(folder_path):
         "average_time_taken_without_rts": 0,
         "average_time_taken_with_rts": 0,
         "average_saved_time": 0,
-        "average_ratio": 0
+        "average_ratio": 0,
+        "average_nr_of_selected_tests": 0,
+        "average_nr_of_changed_files": 0
     }
 
-    with open("../output.csv", "r") as f:
+    with open(f"../{folder_path}-output.csv", "r") as f:
         lines = f.readlines()
-        for line in lines:
+        for idx, line in enumerate(lines):
+            if idx == 0:
+                continue
+
             line = line.split(",")
             AVERAGE_STATISTICS["average_time_taken_without_rts"] += float(line[1])
-            AVERAGE_STATISTICS["average_time_taken_with_rts"] += float(line[2])
-            AVERAGE_STATISTICS["average_saved_time"] += float(line[3])
-            AVERAGE_STATISTICS["average_ratio"] += float(line[4])
+            AVERAGE_STATISTICS["average_nr_of_changed_files"] += float(line[2])
+            AVERAGE_STATISTICS["average_nr_of_selected_tests"] += float(line[3])
+            AVERAGE_STATISTICS["average_time_taken_with_rts"] += float(line[4])
+            AVERAGE_STATISTICS["average_saved_time"] += float(line[5])
+            AVERAGE_STATISTICS["average_ratio"] += float(line[6])
 
     AVERAGE_STATISTICS["average_time_taken_without_rts"] /= len(lines)
     AVERAGE_STATISTICS["average_time_taken_with_rts"] /= len(lines)
     AVERAGE_STATISTICS["average_saved_time"] /= len(lines)
     AVERAGE_STATISTICS["average_ratio"] /= len(lines)
+    AVERAGE_STATISTICS["average_nr_of_selected_tests"] /= len(lines)
+    AVERAGE_STATISTICS["average_nr_of_changed_files"] /= len(lines)
 
     # write to a new file
-    with open("../average_statistics.csv", "w") as f:
+    with open(f"../average_statistics.csv", "w") as f:
         # write headers
         f.write(",".join(AVERAGE_STATISTICS.keys()))
         # write values using f string and \n
         f.write(
-            f"\n{AVERAGE_STATISTICS['average_time_taken_without_rts']},"
+            f"\n{AVERAGE_STATISTICS['file']},"
+            f"{AVERAGE_STATISTICS['average_time_taken_without_rts']},"
+            f"{AVERAGE_STATISTICS['average_nr_of_changed_files']},"
+            f"{AVERAGE_STATISTICS['average_nr_of_selected_tests']},"
             f"{AVERAGE_STATISTICS['average_time_taken_with_rts']},"
             f"{AVERAGE_STATISTICS['average_saved_time']},"
-            f"{AVERAGE_STATISTICS['average_ratio']}")
+            f"{AVERAGE_STATISTICS['average_ratio']}"
+        )
 
 
 def run_test_suite_build_mapping_database():
@@ -154,18 +182,25 @@ def run_test_suite_build_mapping_database():
     # run command in folder
     try:
         os.system("pytest --cov=. --cov-context=test --cov-config=.coveragerc")
+        # rename coverage files
+        os.system("mv .coverage pytest-rts-coverage")
     except Exception as e:
         print("ERROR: Unexpected error: Could not run tests")
 
 
 def __cleanup():
     log.info("Cleaning up the coverage db...")
+    # checkout to master
+    os.system("git checkout master -f")
     # delete file
-    if os.path.exists(".coverage"):
-        os.remove(".coverage")
+    if os.path.exists("pytest-rts-coverage"):
+        os.remove("pytest-rts-coverage")
+
 
     print("Done cleaning up the coverage dbs...")
 
 
 if __name__ == '__main__':
+    # os.chdir(target_folders[0])
+    # calculate_average_statistics("aws-dynamodb-encryption-python")
     init()
